360b2967c19d1dce57982982b1d612efc0d3f601
Anatoly <iamgerman1711@gmail.com>
2022-08-31 13:19:20 +0300

    Lesson_193._Add_React_in_project

---
+++ src/reducer.js
@@ -0,0 +1,42 @@
+const initialState = {value: 0};
+
+const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case 'INC':
+            return {
+                ...state,
+                value: state.value + 1
+            };
+        case 'DEC':
+            return {
+                ...state,
+                value: state.value - 1
+            };
+        case 'RND':
+            return {
+                ...state,
+                value: state.value * action.payload
+            };
+        default:
+            return state;
+    }
+
+}
+
+export default reducer;
+
+// const reducer = (state = 0, action) => {
+
+//     switch (action.type) {
+//         case 'INC':
+//             return state + 1;
+//         case 'DEC':
+//             return state -1;
+//         case 'RND':
+//             return state * action.payload;
+//         default:
+//             return state;
+//     }
+
+// }
